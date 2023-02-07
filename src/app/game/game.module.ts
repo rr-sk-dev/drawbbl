c8c93103474d22fa7d542aef45448168cad29e7c
rtsrodrigues <ruben.rodrigues@productdock.com>
2023-02-07 09:26:40 +0000

    fix color picker

--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { GameRoutingModule } from './game-routing.module';
-import { GameComponent } from './game.component';
+import { SharedModule } from '../shared/shared.module';
+import { ChatComponent } from './components/chat/chat.component';
 import { DrawComponent } from './components/draw/draw.component';
 import { PlayersComponent } from './components/players/players.component';
-import { ChatComponent } from './components/chat/chat.component';
+import { GameRoutingModule } from './game-routing.module';
+import { GameComponent } from './game.component';
 
 @NgModule({
   declarations: [GameComponent, DrawComponent, PlayersComponent, ChatComponent],
-  imports: [CommonModule, GameRoutingModule],
+  imports: [CommonModule, GameRoutingModule, SharedModule],
 })
 export class GameModule {}
